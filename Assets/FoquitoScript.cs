fce91c3e1b0f5270db503dbcbc3caf9bbe0637dd
gabislot07 <gabislot07@gmail.com>
2024-06-02 02:29:27 -0300

    EJERCICOS 1 Y 2 DE TP 06 - Array Foquitos (Repaso prueba)

--- Assets/FoquitoScript.cs
+++ Assets/FoquitoScript.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class FoquitoScript : MonoBehaviour
 {
     [SerializeField] GameObject[] colors;
-    public int currentLightIndex =-1;
-
+    private int currentLightIndex = -1;
+    private int cuentaCiclo = 0; //lleva el ciclo de encendido de luces
+    private const int CicloMax = 3; // variable constante ya que representa la canridad maxima de ciclos de encendido de luces
     void Start()
     {
         
     }
-
+   
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void ActivateNextLight()
@@ -24,23 +25,19 @@ public class FoquitoScript : MonoBehaviour
         if (currentLightIndex >= colors.Length) // si es = o mayor a colors lenght (canridad de colores de un array), luego esta variable vuelve a 0 para que no tire error.
         {
             currentLightIndex = 0;
+            cuentaCiclo++;
+            if (cuentaCiclo >= CicloMax)
+            {
+                Destroy(gameObject); //destrye el foquito que produce los ciclos de  luces
+                return; // Salir de la función para evitar la activación de la luz
+            }
         }
+
         DeactivateAllLights(); //desactiva todas las luces
-        colors[currentLightIndex].SetActive(true); //activa una sola luz, que es el elem,ento que corresponde en la array de colors
+        colors[currentLightIndex].SetActive(true);  //activa una sola luz, que es el elem,ento que corresponde en la array de colors
     }
 
-    public void ActivatePreviousLight() //HACE LO MISMO QUE LA ANTERIOR PERO PARA ATRAS
-    {
-        currentLightIndex--;
-        if (currentLightIndex < 0)
-        {
-            currentLightIndex = colors.Length - 1;
-        }
-        DeactivateAllLights();
-        colors[currentLightIndex].SetActive(true);
-    }
-
-    void DeactivateAllLights()
+    void DeactivateAllLights() //HACE LO MISMO QUE LA ANTERIOR PERO PARA ATRAS_
     {
         foreach (GameObject g in colors)
         {
@@ -51,6 +48,6 @@ public class FoquitoScript : MonoBehaviour
     public void ActivateRepeating(float t)
     {
         CancelInvoke();
-        InvokeRepeating(nameof(ActivateNextLight),0,t);
+        InvokeRepeating(nameof(ActivateNextLight), 0, t);
     }
 }
